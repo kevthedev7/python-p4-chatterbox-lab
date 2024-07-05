1ef32024b35be666b6ba8efd571530f3cc7300e9
Mwenda <kelvinfotech@gmail.com>
2024-07-05 14:51:22 +0300

    Python P4 Chatterbox Lab

--- server/app.py
+++ server/app.py
@@ -14,13 +14,41 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
-def messages():
-    return ''
+# GET /messages: returns an array of all messages as JSON, ordered by created_at in ascending order.
+# POST /messages: creates a new message with a body and username from params, and returns the newly created post as JSON.
+# PATCH /messages/<int:id>: updates the body of the message using params, and returns the updated message as JSON.
+# DELETE /messages/<int:id>: deletes the message from the database.
 
-@app.route('/messages/<int:id>')
-def messages_by_id(id):
-    return ''
+@app.route('/messages', methods=['GET'])
+def messages():
+    if request.method == 'GET':
+        messages = Message.query.order_by(Message.created_at).all()
+    return jsonify([message.to_dict() for message in messages])
+
+@app.route('/messages', methods=['POST'])
+def create_message():
+    if request.method == 'POST':
+        data = request.get_json()
+        new_message = Message(body=data['body'], username=data['username'])
+        db.session.add(new_message)
+        db.session.commit()
+    return jsonify(new_message.to_dict())
+
+@app.route('/messages/<int:id>', methods=['PATCH'])
+def update_messages_by_id(id):
+    if request.method == 'PATCH':
+        message = db.session.get(Message, id)
+        data = request.get_json()
+        message.body = data.get('body', message.body)
+        db.session.commit()
+    return jsonify(message.to_dict())
+
+@app.route('/messages/<int:id>', methods=['DELETE'])
+def delete_messages_by_id(id):
+    if request.method == 'DELETE':
+        message = db.session.get(Message, id)
+        db.session.delete(message)
+        db.session.commit()
 
 if __name__ == '__main__':
     app.run(port=5555)
